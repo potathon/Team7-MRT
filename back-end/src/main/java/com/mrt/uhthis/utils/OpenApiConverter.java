6d4dbb02bb95d82875c2e7003a6c85419f760bfd
grulla79 <wnsgh9826@naver.com>
2024-07-13 22:23:34 +0900

    [BE] feat: openapi 연동

--- back-end/src/main/java/com/mrt/uhthis/utils/OpenApiConverter.java
+++ back-end/src/main/java/com/mrt/uhthis/utils/OpenApiConverter.java
@@ -1,7 +1,10 @@
 package com.mrt.uhthis.utils;
 
 import com.mrt.uhthis.dto.TrashBinResponseDTO;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import java.util.List;
 
 public interface OpenApiConverter {
